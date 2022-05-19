bb8016df14c26d7138f6b74f7fe762ea5aa0dc61
Thanh Nguyen <thanhnguyendhnt95@gmail.com>
2022-05-19 21:57:41 +0700

    update

---
+++ b11/Lesson/main.js
@@ -0,0 +1,42 @@
+//  const diBao = ()=> {
+//     setInterval(()=> {
+//         console.log("Sn Bac");
+//     }, 1000);
+// };
+// function clickMe(){
+//     console.log("Vn vo dich")
+// }
+// const btn = document.getElementById("last-btn");
+// btn.addEventListener("click",()=> {
+//     diBao();
+// });
+// btn.addEventListener("click", function(){
+//     clickMe();
+// });
+// btn.addEventListener("mouseover", ()=>{
+//     console.log("Hover")
+//     btn.classList.add("big");
+// });
+//     btn.addEventListener("mouseout", ()=>{
+//         btn.classList.remove("big");
+//     });
+
+// // onclick
+// // ondbclick
+// // onChange onBlur onFocus
+// //onInput
+// const emailEle = document.getElementById("email");
+// emailEle.addEventListener("input", (event)=>{
+//     console.log("onInput", event.target.value); //onKeypress, onKeyup, onKeydown
+// });
+// window.addEventListener("copy", ()=>{
+//     console.log("123");
+// })
+
+function login(e){
+    console.log(e);
+}
+const formEle = document.getElementById("login-form");
+formEle.addEventListener("",(event) =>{
+
+});
