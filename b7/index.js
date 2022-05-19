73a9313048346f52e2ff15ffb9ee39008d7b2a7a
Thanh Nguyen <thanhnguyendhnt95@gmail.com>
2022-05-19 14:36:24 -0700

    update

---
+++ b7/index.js
@@ -0,0 +1,4 @@
+let myBool = "thanh" = "CHUNG" .toLowerCase();
+let mynotBool = false;
+console.log(myBool);
+console.log(mynotBool);
---
+++ b8/index.js
@@ -0,0 +1,71 @@
+//let A= true;
+//let B= true;
+//let C = false;
+//let myBool= ((A || B) && C) || (B && (A || C));
+// true( && C) || (B && true)
+// false || true
+//let myBool= !!C phu dinh
+//let A= null;
+//let myBool= !!A;
+// false, "", 0, null,undefined
+//console.log(myBool);
+//if (1>0) {
+      //  console.log ("Ket qua sai")
+//}
+//let myStr = "123";
+//console.log(Number(myStr));
+//console.log(typeof Number(myStr));
+//let a= "dieukien1";
+//let A= 10;
+//let B= 20;
+
+
+//if (A>B) { 
+  //  console.log ("A>B");
+//} else if (A === B) {
+  //  console.log ("A=B"); // cung kieu du lieu
+//} else if (A == B) {
+  //      console.log ("A=B"); // khac kieu du lieu
+//}else{
+  //  console.log ("A<B"); // khac kieu du lieu
+//}
+
+//let goToTheHotel = 0;
+//let yourMonth= 5;
+
+//switch (yourMonth) {
+//    case 1:
+//case 2:
+//    case 3:
+//        console.log( "Spring");
+//         break;       
+//    case 4:        
+//    case 5:
+//    case 6:
+//        console.log( "Summer");
+//         break;
+//    default:
+//        console.log("Khong phai la thang");
+//        break;
+//}
+//let goToTheHotel=0;
+//let monthNum= 5;
+//let monthStr= "5";
+//if (monthStr=== monthNum) {
+ //   console.log("= 1");
+//}
+//if (monthStr== monthNum) {
+ //   console.log("= 2");
+//}
+
+//let A=5;
+//let B=7;
+// C= "More than" neu A>B
+// C= "Less" neu A,
+//let C= A> B ? "More ": (A== B ? "Equal" : "Less")
+
+let myStr= "123";
+let myNum1= 10;
+let myNum2= 20;
+let result = myNum1 + myNum2 + myStr;
+console.log("Kieu: ", typeof result, "\nGia tri:", result);
