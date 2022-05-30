91a43f9e9492ff1b565eff6b04a791c5db4c9a33
Thanh Nguyen <thanhnguyendhnt95@gmail.com>
2022-05-30 09:38:57 -0700

    update

---
+++ b10/script.js
@@ -0,0 +1,15 @@
+// Kiem tra so chan nguoi dung nhap vao
+
+// Kiem tra so nguyen to nguoi dung nhap vao
+
+//lay so nhap vao
+// kiem tra du lieu co phai la so ko
+// Convert tu string => number
+ // Kiem tra so chan
+ // Kiem tra so nguyen to
+ function checkInputnumber(inputStr) {
+     // TODO
+     console.log(inputStr);
+     return inputSt
+ }
+ checkInputnumber();
